c3bb4ec1435c5d9bfd43112807cbb3a9fa063136
Dilish P <dilishpothiadath@gmail.com>
2020-05-17 15:21:18 +0530

    Addition of Number Of Island Computation

---
+++ AlgoQs/SurroundedRegionsDFS.cpp
@@ -0,0 +1,102 @@
+#include "pch.h"
+#include <vector>
+
+using namespace std;
+
+//The Idea here is to find all the regions which are not surrounded by X
+//This can be found by checking the boundary O and performing DFS on these 
+//O and making the O discovered to * for later use.
+
+//After this translate the O remaining to X and * to O.
+//Similar Question Number of Islands
+
+void performDFS(vector<vector<char>>& board, int i, int j)
+{
+    if (i < 0 || (i >= board.size()) || j < 0 || (j >= board[0].size()))
+        return;
+
+    if (board[i][j] == 'X')
+        return;
+
+    if (board[i][j] == '*')
+        return;
+
+    if (board[i][j] == 'O')
+        board[i][j] = '*';
+
+    performDFS(board, i, j + 1);
+    performDFS(board, i + 1, j);
+    performDFS(board, i, j - 1);
+    performDFS(board, i - 1, j);
+}
+
+void solve(vector<vector<char>>& board) {
+    for (int i = 0;i < board.size();++i)
+    {
+        for (int j = 0;j < board[i].size();++j)
+        {
+            if (board[i][j] == 'O')
+            {
+                if (i == 0 || i == (board.size() - 1)
+                    || j == 0 || j == (board[0].size() - 1))
+                {
+                    performDFS(board, i, j);
+                }
+            }
+
+        }
+    }
+
+
+    for (int i = 0;i < board.size();++i)
+    {
+        for (int j = 0;j < board[i].size();++j)
+        {
+            if (board[i][j] == '*')
+            {
+                board[i][j] = 'O';
+            }
+            else if (board[i][j] == 'O')
+            {
+                board[i][j] = 'X';
+            }
+        }
+    }
+}
+
+void performDFSII(vector<vector<char>>& board, int i, int j)
+{
+    if (i < 0 || (i >= board.size()) || j < 0 || (j >= board[0].size()))
+        return;
+
+    if (board[i][j] == '0')
+        return;
+
+    if (board[i][j] == 'v')
+        return;
+
+    if (board[i][j] == '1')
+        board[i][j] = 'v';
+
+    performDFS(board, i, j + 1);
+    performDFS(board, i + 1, j);
+    performDFS(board, i, j - 1);
+    performDFS(board, i - 1, j);
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    int islandCount = 0;
+    for (int i = 0;i < grid.size();++i)
+    {
+        for (int j = 0;j < grid[0].size();++j)
+        {
+            if (grid[i][j] == '1')
+            {
+                performDFS(grid, i, j);
+                islandCount++;
+            }
+        }
+    }
+
+    return islandCount;
+}
